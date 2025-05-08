dea8b906e16162bcea3f903644d7c990efa692ee
Brice <briceyan3.0@gmail.com>
2025-05-08 09:52:40 +0800

    feat: add autocmd module and update package.json scripts

---
+++ src/autocmd.ts
@@ -0,0 +1,2 @@
+// ai! create a function to generate a commander command into a commander program, given a function defined in an solidity function(can be found in abis)
+// ai! creat another function to iterater an abi file to retrieve all the functions and call command generation to configure the program passed in
