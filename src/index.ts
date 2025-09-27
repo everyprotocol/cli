bb0e15f69e633d034dbbadf185f490a3e6856d06
Brice <briceyan3.0@gmail.com>
2025-09-27 09:04:12 +0800

    feat: update default links for localnet

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,6 @@ import { program } from "./program.js";
 try {
   await program.parseAsync();
 } catch (e: any /* eslint-disable-line */) {
-  console.error(e.message);
+  console.error(e);
   process.exitCode = 1;
 }
